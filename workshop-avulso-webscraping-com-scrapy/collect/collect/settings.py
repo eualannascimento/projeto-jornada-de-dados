c3c73560ac721d53873c153a0f15095cfacbda10
Alan Nascimento <eualannascimento@gmail.com>
2024-05-26 00:19:36 -0300

    incluindo user_agent para uso do 'scrapy shell'

--- workshop-avulso-webscraping-com-scrapy/collect/collect/settings.py
+++ workshop-avulso-webscraping-com-scrapy/collect/collect/settings.py
@@ -12,7 +12,7 @@ BOT_NAME = "collect"
 SPIDER_MODULES = ["collect.spiders"]
 NEWSPIDER_MODULE = "collect.spiders"
 
-
+USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36'
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
 #USER_AGENT = "collect (+http://www.yourdomain.com)"
 
